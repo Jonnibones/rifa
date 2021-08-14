e24cd800564e235c61db7232cab7e51c97e7a256
Jonnibones <js5800672@gmail.com>
2021-08-13 22:20:44 -0300

    Criada algumas rotas com o slim e preparando para criar uma área de membros

--- index.php
+++ index.php
@@ -2,23 +2,22 @@
 
 require __DIR__.'/vendor/autoload.php';
 require __DIR__.'/vendor/rifacls/src/temps/header.html';
-require __DIR__.'/vendor/rifacls/src/temps/index/mainindex.html';
-require __DIR__.'/vendor/rifacls/src/temps/footer.html';
+
+
 
 $app = new \Slim\Slim();
 
-$app->config('debug', true);
-
 $app->get('/', function() {
-    
-	$teste = new DB\Rifa;
-	$resultados = $teste::Getlist();
-
-	//echo json_encode($resultados);
-	
-
+	require __DIR__.'/vendor/rifacls/src/temps/index/mainindex.html';
 });
 
+$app->get('/minha-conta', function() {
+	require __DIR__.'/vendor/rifacls/src/temps/minha-conta/minha-conta.html';
+});
+$app->get('/user', function() {
+	require __DIR__.'/vendor/rifacls/src/temps/user/user.html';
+});
 $app->run();
 
+require __DIR__.'/vendor/rifacls/src/temps/footer.html';
  ?>
--- minha-conta.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-require __DIR__.'/vendor/autoload.php';
-require __DIR__.'/vendor/rifacls/src/temps/header.html';
-require __DIR__.'/vendor/rifacls/src/temps/minha-conta/minha-conta.html';
-require __DIR__.'/vendor/rifacls/src/temps/footer.html';
-
- ?>
